d0d5cdf2e0ccaed8c58a07f241250c582a73f5e4
Mar <tejedamarcia@gmail.com>
2016-10-23 00:54:23 -0300

    hotfix: Adding headers to receive rest request

--- src/main/java/unq/utils/WebServiceConfiguration.java
+++ src/main/java/unq/utils/WebServiceConfiguration.java
@@ -1,7 +1,11 @@
 package unq.utils;
 
+import spark.Spark;
+
 import java.util.Map;
 
+import static spark.Spark.before;
+import static spark.Spark.options;
 import static spark.Spark.port;
 
 /**
@@ -23,5 +27,25 @@ public class WebServiceConfiguration {
         }
         else port(Integer.valueOf(value.get("PORT")));
         //find a way to set a base url
+
+        options("/*", (request,response)->{
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if(accessControlRequestMethod != null){
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response)->{
+            response.header("Access-Control-Allow-Origin", "*");
+        });
+
     }
 }
